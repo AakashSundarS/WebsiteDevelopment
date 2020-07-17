6cb19a254d568f3eb14713f91838c9963e1dd8e7
Aakash <saakashsundar007@gmail.com>
2020-07-18 01:11:35 +0530

    modified:   db.js 	modified:   index.html 	new file:   login-try.html 	new file:   main.html 	new file:   package-lock.json 	new file:   package.json 	.vscode/ 	database/

--- Sign Up/db.js
+++ Sign Up/db.js
@@ -1,5 +1,27 @@
 let fs = require("fs");
-fs.appendFile("./database/newfile.txt", "Hello content!", function (err) {
-  if (err) throw err;
-  console.log("Saved!");
-});
+function check() {
+  //let name = document.getElementById("name").value;
+  //let password = document.getElementById("password").value;
+  let name = "Aakash";
+  let password = "Password";
+  let search = "./database/" + name + ".json";
+
+  fs.readFile(search, function (err, data) {
+    if (data === password) {
+      console.log("WELCOME!");
+      return true;
+    } else if (err) {
+      console.log("WRONG USERNAME!");
+      //document.getElementById("wrong").innerHTML =
+      //"The username is incorrect!, please try again...";
+      return false;
+    } else {
+      console.log("WRONG PASSWORD!");
+      console.log(data.password);
+      //document.getElementById("wrong").innerHTML =
+      //"The password is incorrect!, please try again...";
+      return false;
+    }
+  });
+}
+check();
